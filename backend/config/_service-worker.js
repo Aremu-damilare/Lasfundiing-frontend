7f1a0f2326a364a4588bd87cfb4c360a8d8ac6d0
aremu-damilare <a.matthew2013@gmail.com>
2023-10-22 11:40:09 +0100

    major updates

--- backend/config/_service-worker.js
+++ backend/config/_service-worker.js
@@ -3,7 +3,7 @@ var APP_VER = '1.1.1';
 var LOCALHOST_BACKEND_URL = `http://127.0.0.1:8000`;
 var LIVEHOST_BACKEND_URL = `https://lasfunding.com:8080`;
 var PRIMARY_COLOR = '#df5b1e'
-var baseUrl = LOCALHOST_BACKEND_URL
+var baseUrl = LIVEHOST_BACKEND_URL
 const accessToken = localStorage.getItem('access_token');
 
 
